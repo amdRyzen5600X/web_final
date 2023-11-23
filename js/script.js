38c4e22ff8a17fab91a8d3699db7390e854c15b2
AMD_RYZEN_5_7600X <arca26100@gmail.com>
2023-11-23 23:02:24 +0600

    add email validation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener('load', () => {
     preloader.remove();
 })
+
+function validateEmail(){
+    let x = document.forms["form"]["email"].value
+    if (String(x).toLowerCase().match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+        alert("accepted!")
+    }
+    else{
+        alert("type a real email")
+    }
+}
